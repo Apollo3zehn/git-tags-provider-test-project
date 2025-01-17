c84545195a9d02829328f080cd440a02166f9111
Vincent Wilms <git@m1.apollo3zehn.net>
2025-01-17 15:49:08 +0100

    Fix style

--- MyLogger.cs
+++ MyLogger.cs
@@ -1,22 +1,21 @@
 using Microsoft.Extensions.Logging;
 
-namespace Foo
+namespace Foo;
+
+public class MyLogger : ILogger
 {
-    public class MyLogger : ILogger
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
     {
-        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return logLevel == LogLevel.Debug;
-        }
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == LogLevel.Debug;
+    }
 
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
-        {
-            throw new NotImplementedException();
-        }
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        throw new NotImplementedException();
     }
 }
